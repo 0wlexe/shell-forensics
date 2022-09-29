d740e1e64ffea6a352c8f8adaa822abbaf272077
Bea <30362709+j4nedoe@users.noreply.github.com>
2022-09-29 00:13:33 -0300

    Add files via upload

---
+++ close-case.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# close-case.sh
+#
+# Simple script to turn off all listeners.
+# Must be used as part of the initial live response.
+# Adapted by nCKbr from the original version by Dr. Phil Polstra - Polstra (2015)
+echo “Turning off listeners at $(date) by user request” | nc localhost 4444
+killall start-case.sh
+killall start-file-listener.sh
+killall nc
